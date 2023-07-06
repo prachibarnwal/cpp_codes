060d97dd37b009c6827a0b1152eef1215eb47f1e
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2023-07-06 18:43:23 +0530

    Add files via upload

---
+++ 77.)sorting elements in an array in descending order using selection sort.cpp
@@ -0,0 +1,40 @@
+//to input an array and define a function to 
+//sort the array in increasing order
+#include<iostream>
+using namespace std;
+void sort(int arr[],int n);
+int main()
+{
+	int n,i,arr[30];
+	cout<<"Enter the size of the array(1-30) : ";
+	cin>>n;
+	cout<<"Enter the numbers : ";
+	for (i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	sort(arr,n);
+	return 0;
+}
+
+void sort(int arr[],int n)
+{
+	int i,j,temp;
+	for (i=0;i<n-1;i++)
+	{
+		for (j=i+1;j<n;j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	cout<<"ARRAY ARRANGED IN DESCENDING ORDER : ";
+	for (i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
