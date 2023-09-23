86ce8560191796e79aa64b75c238f0d08a6b89f6
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2023-09-23 12:08:22 +0530

    Add files via upload

--- 127.) working on arrays using classes.cpp
+++ 127.) working on arrays using classes.cpp
@@ -14,22 +14,41 @@ class Array
 		void show();
 		int search(int);
 		void sort(int);
+		Array add(Array);
 };
 int main()
 {
-	Array p,q;
+	Array p,q,r;
 	p.input();
 	q.input();
 	p.show();
 	q.show();
-	p.sort(1);
-	q.sort(-1);
-	p.show();
-	q.show();
-	cout<<"15 Found at "<<p.search(15)<<endl;
+	r = p.add(q);
+	r.show();
+	//p.sort(1);
+	//q.sort(-1);
+	//p.show();
+	//q.show();
+	//cout<<"15 Found at "<<p.search(15)<<endl;
 	return 0;
 }
-
+Array Array::add(Array t)
+{
+	Array res;
+	if(n == t.n)
+	{
+		res.a = new int[n];
+		res.n = n;
+		for(int i=0;i<n;i++)
+			res.a[i] = a[i] + t.a[i];
+	}
+	else
+	{
+		res.n = 0;
+		cout<<"Size Not Equal"<<endl;
+	}
+	return res;
+}
 void Array::input()
 {
 	cout<<"Enter the Number of Elements : ";
