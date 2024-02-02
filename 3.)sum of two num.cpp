7fcf540cabcfbef501ba7ed2c4d2e5b737cb7d2d
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2024-02-02 23:28:46 +0530

    Add files via upload

---
+++ 3.)sum of two num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter First Number : ";
+	cin>>a;
+	cout<<"Enter Second Number : ";
+	cin>>b;
+	c = a+b;
+	cout<<"SUM OF "<<a<<" and "<<b<<" is "<<c;
+	return 0;
+}
