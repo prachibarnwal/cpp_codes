683c9d3c1a1ea60617868eeb6242bee350d792e2
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2024-02-04 00:54:49 +0530

    Add files via upload

---
+++ 145.) overloading cin and cout in student class.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+	int admno;
+	char name[20];
+	float marks;
+	public:
+		friend istream& operator >>(istream &x, Student &y);
+		friend ostream& operator <<(ostream &x, Student &y);
+};
+
+int main()
+{
+	Student a, b;
+	cin>>a>>b;
+	//cin>>b;
+	cout<<a<<b;
+	//cout<<b;
+	return 0;
+}
+
+istream& operator >>(istream &x, Student &y)
+{
+	cout<<"Enter Admission Number : ";
+	cin>>y.admno;
+	cout<<"Enter Name : ";
+	cin>>y.name;
+	cout<<"Enter Marks : ";
+	cin>>y.marks;
+	return x;
+}
+ostream& operator <<(ostream &x, Student &y)
+{
+	cout<<"Admission Number is : "<<y.admno<<endl;
+	cout<<"Name is : "<<y.name<<endl;
+	cout<<"Marks is : "<<y.marks<<endl;
+	return x;
+}
